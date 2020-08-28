f85db576a2fcdf839bc303f2ed920ea7cbc7428f
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-08-28 11:51:53 +0200

    Add react media;
    Display message when orientation on smaller devices is landscape;

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import styled from "styled-components"
+import { StyledLink } from "../reuse/Button";
 import Button from "../reuse/Button";
 import H1 from "../reuse/H1";
+import { ButtonsWrapper, SelectPage } from "./Select-style";
+import Media from "react-media";
+import Warning from "../ScreenWarning/Warning";
+import Loading from '../Loading/Loading';
 
-const ButtonsWrapper = styled.div`
-height: 70%;
-button{
-    margin-top: 35px;
-    margin-left: 50%;
-    transform: translateX(-50%)
-}
-`
+import searching from "../../assets/img/searching.png"
 
 function Select() {
     return (
-        <>
-            <H1>Which genre do you choose?</H1>
-            <ButtonsWrapper>
-                <Button>Horror</Button>
-                <Button>Comedy</Button>
-                <Button>Romance</Button>
-                <Button>Fantasy</Button>
-            </ ButtonsWrapper>
-        </>
+        <Media query="( orientation: landscape ) and ( max-width: 1023px )"    >
+            {matches => (
+                matches ? <Warning /> :
+                    <SelectPage>
+                        <H1>Which genre do you choose?</H1>
+                        <ButtonsWrapper>
+                            <StyledLink to="/movie-app/"><Button>Horror</Button></StyledLink>
+                            <StyledLink to="/movie-app/"><Button>Comedy</Button></StyledLink>
+                            <StyledLink to="/movie-app/"><Button>Romance</Button></StyledLink>
+                            <StyledLink to="/movie-app/"><Button>Fantasy</Button></StyledLink>
+                        </ ButtonsWrapper>
+                    </SelectPage>
+            )}
+        </ Media>
     )
 }
 export default Select
