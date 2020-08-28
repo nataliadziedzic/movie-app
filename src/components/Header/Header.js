bfa9ac6a18d73bfb96787663ca23f92ee6c1cc75
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-08-28 12:35:33 +0200

    Change pictures to a larger size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,24 +3,29 @@ import styled from "styled-components"
 import logo from "../../assets/img/logo.png"
 const LogoContainer = styled.header`
 height: 15vh;
+min-height: auto
 @media (orientation: landscape)  and (max-width: 1023px){
-    height: 25vh
+    height: 35vh
+}
+@media (orientation: landscape)  and (min-width: 1200px){
+    height: 20vh
 }
 `
 const Logo = styled.div`
 height: 100%;
     img{
-        margin: 15px;
+        margin: 10px -5px;
+        transform: scale(0.8);
     }
    @media (min-width: 720px){
        img{
-           transform: scale(1.2);
-           margin: 28px 0 0 35px;
+           transform: scale(1);
+           margin: 20px 0 0 30px;
        }
    }
    @media (min-width: 1024px){
     img{
-        transform: scale(1.3);
+        transform: scale(1.2);
         margin: 28px 0 0 55px;
     }
 }
