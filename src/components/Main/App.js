35d8c8e606ca24a73b4b7df1530f6ef49c6958e8
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-08-28 12:39:18 +0200

    Correct sizing

--- src/components/Main/App.js
+++ src/components/Main/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Section from "../Section/Section";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+    window.addEventListener("orientationchange", function () {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  })
   return (
     <>
       <Header />
