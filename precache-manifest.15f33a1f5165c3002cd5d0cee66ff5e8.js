a6bd085592afd9cb7022c4974b37c9d6a1765dc3
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-09-03 17:13:42 +0200

    Updates

---
+++ precache-manifest.15f33a1f5165c3002cd5d0cee66ff5e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c66c9fa222d6095f6ab3eb4c69411d",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "42425b30312bf541b183",
+    "url": "/movie-app/static/js/2.8d9c50db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.8d9c50db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be021112db76f0735a59",
+    "url": "/movie-app/static/js/main.6031eb95.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  },
+  {
+    "revision": "ea8154fbec4885e7259649c22040d945",
+    "url": "/movie-app/static/media/main-picture2.ea8154fb.png"
+  }
+]);
